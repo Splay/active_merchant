32837b76d1bc8875618cdfc84f83d4eeb506774f
Guillaume Maury <giom@gom-jabbar.org>
2019-06-27 01:48:22 +0800

    updated to ignore blank states

--- lib/active_merchant/billing/gateways/cyber_source.rb
+++ lib/active_merchant/billing/gateways/cyber_source.rb
@@ -556,7 +556,7 @@ module ActiveMerchant #:nodoc:
           xml.tag! 'street1',               address[:address1]
           xml.tag! 'street2',               address[:address2]                unless address[:address2].blank?
           xml.tag! 'city',                  address[:city]
-          xml.tag! 'state',                 address[:state] || ""
+          xml.tag! 'state',                 address[:state]                   unless address[:state].blank?
           xml.tag! 'postalCode',            address[:zip]
           xml.tag! 'country',               lookup_country_code(address[:country]) unless address[:country].blank?
           xml.tag! 'company',               address[:company]                 unless address[:company].blank?
